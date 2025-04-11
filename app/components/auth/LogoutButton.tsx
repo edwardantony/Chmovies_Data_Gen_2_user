096c59c2aaceb26709437c9037a7986ab6549606
priyeshantony <priyeshantony@gmail.com>
2025-04-11 12:13:17 +0530

    Update LogoutButton.tsx

--- app/components/auth/LogoutButton.tsx
+++ app/components/auth/LogoutButton.tsx
@@ -1,10 +1,12 @@
-// components/LogoutButton.tsx
+'use client';
+
 import { signOut } from 'aws-amplify/auth';
 import { Button } from '../ui/Button';
-import { useRouter } from 'next/router';
+import { useRouter } from 'next/navigation';
 
 export default function LogoutButton() {
   const router = useRouter();
+
   const handleLogout = async () => {
     await signOut();
     router.push('/');
