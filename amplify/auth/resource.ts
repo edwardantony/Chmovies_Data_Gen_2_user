1b0d6d8e36fdf9b121418c6496def14d69e56ad2
priyeshantony <priyeshantony@gmail.com>
2025-04-26 19:25:53 +0530

    Update resource.ts

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -2,7 +2,7 @@ import { referenceAuth } from '@aws-amplify/backend';
 
 export const auth = referenceAuth({
   userPoolId: 'ap-south-1_VNTh0GiXY',
-  identityPoolId: 'ap-south-1:f1f3be8a-a1d3-404b-8d98-4ea82dd634b7',
+  identityPoolId: 'ap-south-1:95a2602c-7b78-42db-846c-1188d278a034',
   authRoleArn: 'arn:aws:iam::252476278316:role/service-role/Gen_2_Authorized_Role',
   unauthRoleArn: 'arn:aws:iam::252476278316:role/service-role/Gen_2_Un_Authorized_Role',
   userPoolClientId: '6n4f8oko78s1k3v3tk3hisrqad',
